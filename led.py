afeda95c775f6db773ebab9852cd0685fd9d9b82
Aaron Wells <aaronw@catalyst.net.nz>
2014-09-15 16:42:20 +1200

    Adding ability to specify graphics as ASCII art instead of raw hex

--- led.py
+++ led.py
@@ -29,63 +29,144 @@ diamond = [
     [0x00, 0x06, 0xFF,0xFE,0xFF,],
 ]
 
-# an unblinking eye
-eye0 = [
-    [0x00, 0x00, 0xFF,0xF1,0xFF, 0xFF,0xEE,0xFF,],
-    [0x00, 0x02, 0xFF,0xDF,0x7F, 0xFF,0xDB,0x7F,],
-    [0x00, 0x04, 0xFF,0xDF,0x7F, 0xFF,0xEE,0xFF,],
-    [0x00, 0x06, 0xFF,0xF1,0xFF,],
-]
 
-eye1 = [
-    [0x00, 0x00, 0xFF,0xFF,0xFF, 0xFF,0xE0,0xFF,],
-    [0x00, 0x02, 0xFF,0xDF,0x7F, 0xFF,0xDB,0x7F,],
-    [0x00, 0x04, 0xFF,0xDF,0x7F, 0xFF,0xE0,0xFF,],
-    [0x00, 0x06, 0xFF,0xFF,0xFF],
-]
+eyes1 = \
+"....xxx.......xxx...." \
+"...x...x.....x...x..." \
+"..x.....x...x.....x.." \
+"..x..x..x...x..x..x.." \
+"..x.....x...x.....x.." \
+"...x...x.....x...x..." \
+"....xxx...x...xxx...." 
 
-eye2 = [
-    [0x00, 0x00, 0xFF,0xFF,0xFF, 0xFF,0xFF,0xFF,],
-    [0x00, 0x02, 0xFF,0xE0,0xFF, 0xFF,0xDB,0x7F,],
-    [0x00, 0x04, 0xFF,0xE0,0xFF, 0xFF,0xFF,0xFF,],
-    [0x00, 0x06, 0xFF,0xFF,0xFF],
-]
+eyes2 = \
+"....................." \
+"...xxxxx.....xxxxx..." \
+"..x.....x...x.....x.." \
+"..x..x..x...x..x..x.." \
+"..x.....x...x.....x.." \
+"...xxxxx.....xxxxx..." \
+"..........x.........." 
 
-eye3 = [
-    [0x00, 0x00, 0xFF,0xFF,0xFF, 0xFF,0xFF,0xFF,],
-    [0x00, 0x02, 0xFF,0xFF,0xFF, 0xFF,0xC0,0x7F,],
-    [0x00, 0x04, 0xFF,0xFF,0xFF, 0xFF,0xFF,0xFF,],
-    [0x00, 0x06, 0xFF,0xFF,0xFF],
-]
+eyes3 = \
+"....................." \
+"....................." \
+"...xxxxx.....xxxxx..." \
+"..x..x..x...x..x..x.." \
+"...xxxxx.....xxxxx..." \
+"....................." \
+"..........x.........." 
 
-eyeblink = [eye0, eye1, eye2, eye3, eye2, eye1, eye0]
+eyes4 = \
+"....................." \
+"....................." \
+"....................." \
+"..xxxxxxx...xxxxxxx.." \
+"....................." \
+"....................." \
+"..........x.........." 
 
-data = diamond
+
+eye1 = \
+".........xxx........." \
+"........x...x........" \
+".......x.....x......." \
+".......x..x..x......." \
+".......x.....x......." \
+"........x...x........" \
+".........xxx........." 
+
+eye2 = \
+"....................." \
+"........xxxxx........" \
+".......x.....x......." \
+".......x..x..x......." \
+".......x.....x......." \
+"........xxxxx........" \
+"....................." 
+         
+eye3 = \
+"....................." \
+"....................." \
+"........xxxxx........" \
+".......x..x..x......." \
+"........xxxxx........" \
+"....................." \
+"....................." 
+
+eye4 = \
+"....................." \
+"....................." \
+"....................." \
+".......xxxxxxx......." \
+"....................." \
+"....................." \
+"....................." 
+
+eyeblink = [eye1, eye2, eye3, eye4, eye3, eye2, eye1]
+eyesblink = [eyes1, eyes2, eyes3, eyes4, eyes3, eyes2, eyes1]
+
+def parse(screen):
+    # strip out all newlines, in case they were using those to format the string
+    screen = screen.replace("\n", '').lower()
+    bytes = []
+    for i in range(7):
+        rowbytes = []
+        # Grab a slice of 21 characters, and pad it out to 24 by adding three 'x's to the end
+        row = screen[(0+i*21):(21+i*21)] + '...'
+        # Now grap slices of that, 8 characters at a time, and convert them to bytes
+        bytenum = 0
+        byteval = 0
+#        print "row: " + row
+        for j in range(24):
+            bitnum = j % 8
+            if (row[j] != 'x'):
+                bitval = 2 ** bitnum
+            else:
+                bitval = 0
+            byteval += bitval
+#            print "char {0} = {1}; bitval = {2}; byteval = {3} ({3:X})".format((j), row[j], bitval, byteval)
+            
+            if bitnum == 7:
+                bytenum += 1
+                rowbytes.insert(0, byteval)
+                byteval = 0
+        bytes.append(rowbytes)
+    
+    #Add an empty "row 8" to round off the size of the fourth packet
+    bytes.append([0xFF] * 3)
+
+    packets = []
+    for i in range(0,7,2):
+        packet = []
+        # brightness (0, 1, or 2; 0 = brightest)
+        packet.append(0)
+        # row number
+        packet.append(i)
+        for j in range(2):
+            for k in range(3):
+                packet.append(bytes[i+j][k])
+        packets.append(packet)
+    return packets
+#        print "\n"
+
+# Data should be an array of packets ready to send to the device
+def sendpackets(data):
+    for packet in data:
+        device.ctrl_transfer(
+            bmRequestType = 0x21,
+            bRequest = 0x09,
+            wValue = 0x0000,
+            wIndex = 0x0000,
+            data_or_wLength = packet
+        )
 
 while (1):
-    
-    for data in eyeblink:
-    
-        for packet in data:
-            device.ctrl_transfer(
-                bmRequestType = 0x21,
-                bRequest = 0x09,
-                wValue = 0x0000,
-                wIndex = 0x0000,
-                data_or_wLength = packet
-            )
-        time.sleep(0.05)
-
     for i in range (0, random.randint(5,25)):
-
-        for packet in data:
-            device.ctrl_transfer(
-                bmRequestType = 0x21,
-                bRequest = 0x09,
-                wValue = 0x0000,
-                wIndex = 0x0000,
-                data_or_wLength = packet
-            )
-        
+        sendpackets(parse(eyes1))
         time.sleep(0.4)
+
+    for data in eyesblink:
+        sendpackets(parse(data))
+        time.sleep(0.05)
 
