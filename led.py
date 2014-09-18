33337430cb45434fe68c8b70c26ef4189e9b29af
Aaron Wells <aaronw@catalyst.net.nz>
2014-09-19 00:18:54 +1200

    Adding command-line params to consolidate the three scripts into one
    And adding a grumpy cat face.

--- led.py
+++ led.py
@@ -1,6 +1,7 @@
 import usb.core
 import time
 import random
+import argparse
 
 # Python program to control the Dream Cheeky USB LED: http://www.dreamcheeky.com/led-message-board
 # Code inspired by dcled: http://www.last-outpost.com/~malakai/dcled/
@@ -16,17 +17,18 @@ diamond = [
     [0x00, 0x06, 0xFF,0xFE,0xFF,],
 ]
 
-
-eyes1 = \
+kitty = []
+kitty.append(
 "..xxx...........xxx.." \
 ".x...x.........x...x." \
 "x.....x.......x.....x" \
 "x..x..x.......x..x..x" \
 "x.....x.......x.....x" \
 ".x...x...xxx...x...x." \
-"..xxx.....x.....xxx.." 
+"..xxx.....x.....xxx.."
+)
 
-eyes2 = \
+kitty.append(
 "....................." \
 ".xxxxx.........xxxxx." \
 "x.....x.......x.....x" \
@@ -34,8 +36,9 @@ eyes2 = \
 "x.....x.......x.....x" \
 ".xxxxx...xxx...xxxxx." \
 "..........x.........." 
+)
 
-eyes3 = \
+kitty.append(
 "....................." \
 "....................." \
 ".xxxxx.........xxxxx." \
@@ -43,8 +46,9 @@ eyes3 = \
 ".xxxxx.........xxxxx." \
 ".........xxx........." \
 "..........x.........." 
+)
 
-eyes4 = \
+kitty.append(
 "....................." \
 "....................." \
 "....................." \
@@ -52,46 +56,90 @@ eyes4 = \
 "....................." \
 ".........xxx........." \
 "..........x.........." 
+)
 
+grumpy = []
+grumpy.append(
+"..xx.....xxx.....xx.." \
+"..x.x.....x.....x.x.." \
+".x..x...........x..x." \
+".x.x.x.........x.x.x." \
+".x...x.........x...x." \
+"..x.x....xxx....x.x.." \
+"..xxx...x...x...xxx.." 
+)
 
-eye1 = \
+grumpy.append(
 ".........xxx........." \
+"..xxx.....x.....xxx.." \
+".x...x.........x...x." \
+".x.x.x.........x.x.x." \
+".x...x.........x...x." \
+"..xxx....xxx....xxx.." \
+"........x...x........" 
+)
+
+grumpy.append(
+".........xxx........." \
+"..........x.........." \
+"..xxx...........xxx.." \
+".x.x.x.........x.x.x." \
+"..xxx...........xxx.." \
+".........xxx........." \
+"........x...x........" 
+)
+
+grumpy.append(
+".........xxx........." \
+"..........x.........." \
+"....................." \
+".xxxxx.........xxxxx." \
+"....................." \
+".........xxx........." \
+"........x...x........" 
+)
+
+
+single = []
+single.append(
+".........xxx........." \
+".........x.x........." \
 "........x...x........" \
-".......x.....x......." \
-".......x..x..x......." \
-".......x.....x......." \
+"........x.x.x........" \
 "........x...x........" \
+".........x.x........." \
 ".........xxx........." 
+)
 
-eye2 = \
+single.append(
 "....................." \
-"........xxxxx........" \
-".......x.....x......." \
-".......x..x..x......." \
-".......x.....x......." \
-"........xxxxx........" \
+"..........x.........." \
+".........x.x........." \
+"........x.x.x........" \
+".........x.x........." \
+"..........x.........." \
 "....................." 
-         
-eye3 = \
-"....................." \
-"....................." \
-"........xxxxx........" \
-".......x..x..x......." \
-"........xxxxx........" \
-"....................." \
-"....................." 
+)
 
-eye4 = \
+single.append(
 "....................." \
 "....................." \
-"....................." \
-".......xxxxxxx......." \
-"....................." \
+".........xxx........." \
+"........x.x.x........" \
+".........xxx........." \
 "....................." \
 "....................." 
+)
 
-eyeblink = [eye1, eye2, eye3, eye4, eye3, eye2, eye1]
-eyesblink = [eyes1, eyes2, eyes3, eyes4, eyes3, eyes2, eyes1]
+single.append(
+"....................." \
+"....................." \
+"....................." \
+"........xxxxx........" \
+"....................." \
+"....................." \
+"....................." 
+)
 
 def parse(screen):
     # strip out all newlines, in case they were using those to format the string
@@ -138,7 +186,10 @@ def parse(screen):
 #        print "\n"
 
 # Data should be an array of packets ready to send to the device
-def sendpackets(data):
+def sendtoled(screen):
+    global device
+    
+    data = parse(screen)
     for packet in data:
         device.ctrl_transfer(
             bmRequestType = 0x21,
@@ -148,56 +199,110 @@ def sendpackets(data):
             data_or_wLength = packet
         )
 
-# find our device
-device = usb.core.find(
-#    backend = usb.backend.libusb0.get_backend(),
-    idVendor = 0x1d34,
-    idProduct = 0x0013
-)
+def usbinit():
+    global device
+    
+    # find our device
+    device = usb.core.find(
+    #    backend = usb.backend.libusb0.get_backend(),
+        idVendor = 0x1d34,
+        idProduct = 0x0013
+    )
 
-detachkerneldriver = True
-
-# Check whether we need to detach the kernel driver
-try:
-    if device.is_kernel_driver_active(0) == False:
-        detachkerneldriver = False
-# In some supported pyusb backend libraries, this method isn't implemented
-# In which case, we may as well just try to detach the kernel driver.
-except NotImplementedError:
     detachkerneldriver = True
 
-if detachkerneldriver == True:
+    # Check whether we need to detach the kernel driver
     try:
-        device.detach_kernel_driver(0)
-    # If we're trying to detach the kernel driver with no information (because
-    # the checking method wasn't implemented), it'll throw this error
-    except usb.core.USBError:
-        pass
+        if device.is_kernel_driver_active(0) == False:
+            detachkerneldriver = False
+    # In some supported pyusb backend libraries, this method isn't implemented
+    # In which case, we may as well just try to detach the kernel driver.
+    except NotImplementedError:
+        detachkerneldriver = True
 
-# try:
-#     device.detach_kernel_driver(0)
-# except usb.core.USBError:
-#     pass
+    if detachkerneldriver == True:
+        try:
+            device.detach_kernel_driver(0)
+        # If we're trying to detach the kernel driver with no information (because
+        # the checking method wasn't implemented), it'll throw this error
+        except usb.core.USBError:
+            pass
 
-def blink():
-    for frame in eyesblink:
-        sendpackets(parse(frame))
-        time.sleep(0.05)
 
-# Wake up by blinking a few times
-for i in range(0, random.randint(2,5)):
-    blink()
+def blink(eyeset, blinktime = 0.05):
+    for frame in eyeset + eyeset[::-1]:
+        sendtoled(frame)
+        time.sleep(blinktime)
 
-# Awake
-while (1):
+######################################
 
-    # Eyes open for random duration
-    for i in range (0, random.randint(0,16)):
+# The available eye sets
+eyesets = {
+    'grumpy' : grumpy,
+    'kitty' : kitty,
+    'single' : single
+}
+
+# Get the command-line options to decide which set of eyes to display, and open or shut
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '-e', '--eyes', 
+    default = 'kitty',
+    choices = sorted(eyesets.keys()),
+    help = 'Which set of eyes to show'
+)
+parser.add_argument(
+    '--shut',
+    action = 'store_true',
+    help = 'Add this to display with eyes shut'
+)
+
+args = parser.parse_args()
+eyes = eyesets[args.eyes]
+
+usbinit()
+
+if (args.shut):
+    blinktime = 0.1
+
+    # Blink a few times before closing your eyes
+    for i in range(0, random.randint(2,4)):
+        blink(eyes, blinktime)
+
+    # Close your eyes (skip the first frame because we showed it during the blink
+    for frame in eyes[1:]:
+        sendtoled(frame)
+        time.sleep(blinktime)
+
+    # Awake
+    while (1):
+
         # Even though the image isn't changing, we need to refresh the LED
         # about every 0.4 seconds
-        sendpackets(parse(eyes1))
+        sendtoled(eyes[-1])
         time.sleep(0.4)
+else:
+    blinktime = 0.05
+    
+    # Open your eyes
+    for frame in eyes[::-1]:
+        sendtoled(frame)
+        time.sleep(blinktime)
 
-    # Blink animation
-    blink()
+    # Wake up by blinking a few times
+    for i in range(0, random.randint(2,5)):
+        blink(eyes, blinktime)
+
+    # Awake
+    while (1):
+
+        # Eyes open for random duration
+        for i in range (0, random.randint(0,16)):
+            # Even though the image isn't changing, we need to refresh the LED
+            # about every 0.4 seconds
+            sendtoled(eyes[0])
+            time.sleep(0.4)
+
+        # Blink animation
+        blink(eyes, blinktime)
 
--- watchlock.sh
+++ watchlock.sh
@@ -1,9 +1,11 @@
 #/bin/sh
 
+eyes=kitty
+
 dbus-monitor --session "interface=org.mate.ScreenSaver,member=ActiveChanged" --monitor | (
 
     echo "Waking up!"
-    python /home/aaronw/programs/pydcled/eyesopen.py &
+    python /home/aaronw/programs/pydcled/led.py -e $eyes &
     LEDPID=$!
 
     function clean_up {
@@ -19,12 +21,12 @@ dbus-monitor --session "interface=org.mate.ScreenSaver,member=ActiveChanged" --m
         if echo $X | grep "boolean true" &> /dev/null; then
             echo "Going to sleep!"
             kill $LEDPID &> /dev/null 2>&1
-            python /home/aaronw/programs/pydcled/eyesshut.py &
+            python /home/aaronw/programs/pydcled/led.py -e $eyes --shut &
             LEDPID=$!
         elif echo $X | grep "boolean false" &> /dev/null; then
             echo "Waking up!"
             kill $LEDPID &> /dev/null 2>&1
-            python /home/aaronw/programs/pydcled/eyesopen.py &
+            python /home/aaronw/programs/pydcled/led.py -e $eyes &
             LEDPID=$!
         fi
     done
