5df6d246c4d0d09e30fc6eb598bf12ef6982e9f7
Aaron Wells <aaronw@catalyst.net.nz>
2014-09-26 12:56:27 +1200

    Rename led.py since it's no longer holding the generic led control code

--- ledbyscreensaver.sh
+++ ledbyscreensaver.sh
@@ -1,11 +1,19 @@
 #/bin/sh
 
-eyes=${1:-kitty}
+# This script will make the LED show open eyes when your computer is active, and
+# closed eyes when your screensaver comes on.
 
-dbus-monitor --session "interface=org.mate.ScreenSaver,member=ActiveChanged" --monitor | (
+eyes=${1:-kitty}
+pathtoeyes=/home/aaronw/programs/pydcled
+# For Mint MATE
+screensaverintf="interface=org.mate.ScreenSaver,member=ActiveChanged"
+# For Gnome
+#screensaverintf="interface=org.gnome.ScreenSaver,member=ActiveChanged"
+
+dbus-monitor --session $screensaverintf --monitor | (
 
     echo "Waking up!"
-    python /home/aaronw/programs/pydcled/led.py -e $eyes &
+    python $pathtoeyes/eyes.py -e $eyes &
     LEDPID=$!
 
     function clean_up {
@@ -21,12 +29,12 @@ dbus-monitor --session "interface=org.mate.ScreenSaver,member=ActiveChanged" --m
         if echo $X | grep "boolean true" &> /dev/null; then
             echo "Going to sleep!"
             kill $LEDPID &> /dev/null 2>&1
-            python /home/aaronw/programs/pydcled/led.py -e $eyes --shut &
+            python $pathtoeyes/eyes.py -e $eyes --shut &
             LEDPID=$!
         elif echo $X | grep "boolean false" &> /dev/null; then
             echo "Waking up!"
             kill $LEDPID &> /dev/null 2>&1
-            python /home/aaronw/programs/pydcled/led.py -e $eyes &
+            python $pathtoeyes/eyes.py -e $eyes &
             LEDPID=$!
         fi
     done
